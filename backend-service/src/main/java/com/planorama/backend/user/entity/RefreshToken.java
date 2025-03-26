ef1d20b52934e515eaa436639035ca290e6b47c6
Matan Leibovich <matan6142@gmail.com>
2025-03-26 19:40:40 +0200

    Change User RefreshToken.java to JWT token

--- backend-service/src/main/java/com/planorama/backend/user/entity/RefreshToken.java
+++
@@ -1,6 +0,0 @@
-package com.planorama.backend.user.entity;
-
-import java.util.UUID;
-
-public record RefreshToken(UUID value, long expireTimeEpochMillis) {
-}
--- backend-service/src/main/java/com/planorama/backend/user/mapper/UserMapper.java
+++ backend-service/src/main/java/com/planorama/backend/user/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.planorama.backend.user.mapper;
 
 import com.planorama.backend.user.api.LoginUserDTO;
-import com.planorama.backend.user.entity.RefreshToken;
 import com.planorama.backend.user.entity.UserDAO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,7 +9,7 @@ import org.mapstruct.Mapping;
 public interface UserMapper {
     @Mapping(target = "id", source = "user.id")
     @Mapping(target = "email", source = "user.email")
-    @Mapping(target = "refreshToken", source = "refreshToken.value")
+    @Mapping(target = "refreshToken", source = "refreshToken")
     @Mapping(target = "accessToken", source = "accessToken")
-    LoginUserDTO daoWithTokensToLoginDto(UserDAO user, String accessToken, RefreshToken refreshToken);
+    LoginUserDTO daoWithTokensToLoginDto(UserDAO user, String accessToken, String refreshToken);
 }
