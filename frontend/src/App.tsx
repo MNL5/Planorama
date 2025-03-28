176e109cbd9fdb4ab6178f2508bbd0b4c32a5720
Matan Leibovich <linormirit@gmail.com>
2025-03-28 07:55:46 +0300

    update font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 import { Routes, Route } from "react-router-dom";
 import { createTheme, MantineProvider } from "@mantine/core";
 
 import { SignIn } from "./components/sign-in/sign-in.tsx";
 import { SignUp } from "./components/sign-up/sign-up.tsx";
-import { Overview } from './components/overview/overview.tsx';
+import { Overview } from "./components/overview/overview.tsx";
 import { HomePage } from "./components/home-page/home-page.tsx";
 
 const theme = createTheme({
-  fontFamily: "Roboto, sans-serif",
+  fontFamily: "Quicksand, sans-serif",
 });
 
 const App: React.FC = () => {
