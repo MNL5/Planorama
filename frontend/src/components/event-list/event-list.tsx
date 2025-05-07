07610b1739aec63fafd1fd065d78b528fb89fdac
Matan Leibovich <linormirit@gmail.com>
2025-05-07 14:43:42 +0300

    final design on event list page

--- frontend/src/components/event-list/event-list.tsx
+++ frontend/src/components/event-list/event-list.tsx
@@ -4,9 +4,9 @@ import {
   Text,
   Group,
   Title,
+  Stack,
   Image,
   Badge,
-  Stack,
   Button,
 } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
@@ -30,60 +30,66 @@ const EventList: React.FC = () => {
   };
 
   return (
-    <Stack w={"100%"} h={"100vh"} gap={"lg"} align={"center"}>
-      <Flex w={"100%"} justify={"flex-end"} style={{ padding: "20px 5%" }}>
+    <Stack w={"100%"} h={"100vh"}>
+      <Flex
+        mt={20}
+        w={"100%"}
+        pos={"relative"}
+        align={"center"}
+        justify={"center"}
+      >
+        <Title order={1}>Your Events</Title>
         <Button
           size={"md"}
           radius={"md"}
           color={"primary"}
           onClick={handleCreateNewEvent}
+          style={{ position: "absolute", right: 20 }}
         >
           + Create New Event
         </Button>
       </Flex>
       <Flex
         gap={"lg"}
-        w={"100%"}
-        h={"100vh"}
         wrap={"wrap"}
         justify={"center"}
-        align={"center"}
+        align={"flex-start"}
+        style={{ marginTop: "20px" }}
       >
         {eventsList?.map((eventItem) => (
           <Card
             w={400}
             key={eventItem.id}
-            shadow={"sm"}
-            padding={"lg"}
-            radius={"md"}
+            shadow="sm"
+            padding="lg"
+            radius="md"
             withBorder
           >
             <Card.Section>
               <Image mah={240} src={eventItem.invitationImg} />
             </Card.Section>
-            <Group justify={"space-between"} mt={"md"} mb={"xs"}>
-              <Title size={"xl"}>{eventItem.name}</Title>
-              <Badge color={"primary.2"}>
+            <Group justify="space-between" mt="md" mb="xs">
+              <Title size="xl">{eventItem.name}</Title>
+              <Badge color="primary.2">
                 {new Date(eventItem.time).toLocaleDateString()}
               </Badge>
             </Group>
 
-            <Text size={"sm"} c={"dimmed"}>
+            <Text size="sm" c="dimmed">
               {eventItem.invitationText}
             </Text>
 
             <Button
-              color={"primary"}
+              color="primary"
               fullWidth
-              mt={"md"}
-              radius={"md"}
+              mt="md"
+              radius="md"
               onClick={() => handleSelectEvent(eventItem)}
             >
               Select Event
             </Button>
           </Card>
         ))}
-        ,
       </Flex>
     </Stack>
   );
