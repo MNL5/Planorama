b67094837cc6d2786a718de17e102877af28b580
Gal Naor <galnaor86@gmail.com>
2025-06-07 19:01:10 +0300

    add to do list

---
+++ frontend/src/services/TaskService/TaskService.ts
@@ -0,0 +1,37 @@
+import { AxiosResponse } from 'axios';
+import Task from '../../types/task';
+import {
+    abortableDeleteRequest,
+    abortableGetRequest,
+    abortablePostRequest,
+    abortablePutRequest,
+} from '../abortable-request';
+
+const getAllTasks = async (eventId: string) => {
+    const response: AxiosResponse<Task[]> = await abortableGetRequest<Task[]>(
+        `tasks?event=${eventId}`
+    ).request;
+    return response.data;
+};
+
+const createTask = async (task: Omit<Task, 'id'>) => {
+    const response = await abortablePostRequest<Task>('tasks', {
+        ...task,
+    }).request;
+    return response.data;
+};
+
+const updateTask = async (task: Task) => {
+    const response = await abortablePutRequest<Task>(`tasks/${task.id}`, {
+        description: task.description,
+        fulfilled: task.fulfilled,
+    }).request;
+    return response.data;
+};
+
+const deleteTask = async (taskId: string) => {
+    const response = await abortableDeleteRequest<Task>(`tasks/${taskId}`)
+        .request;
+    return response.data;
+};
+export { getAllTasks, createTask, updateTask, deleteTask };
---
+++ frontend/src/types/task.ts
@@ -0,0 +1,8 @@
+interface Task {
+    id: string;
+    eventId: string;
+    description: string;
+    fulfilled: boolean;
+}
+
+export default Task;
